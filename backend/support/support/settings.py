89614c22e0c13db697df1949b8c8fc216d0b2517
vikassrivastava18 <vikassrinitb@gmail.com>
2026-08-04 01:43:11 +0530

    Remove debug in production

--- backend/support/support/settings.py
+++ backend/support/support/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-rb8qs")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -86,7 +86,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'support.wsgi.application'
-
 
 
 DATABASES = {
